0c2576d5da039531894b1edb16ae359d04a5e2cc
bonojuno <gtx540@naver.com>
2022-11-09 19:25:26 +0900

    #21 - 게시글 페이지, 댓글 기능 테스트 정의
    dto를 추가로 만들고 그에 대한 테스트를 간단하게 작성함 
    아직 비지니스 로직이 구현이 안되었으므로 테스트는 
    전부 실패한다

--- project-board/src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
+++ project-board/src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
@@ -1,7 +1,9 @@
 package com.fastcampus.projectboard.service;
 
+import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.type.SearchType;
 import com.fastcampus.projectboard.dto.ArticleDto;
+import com.fastcampus.projectboard.dto.ArticleUpdateDto;
 import com.fastcampus.projectboard.repository.ArticleRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,9 +13,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
 
 @DisplayName("비지니스 로직 - 게시글")
 @ExtendWith(MockitoExtension.class)
@@ -49,5 +53,44 @@ class ArticleServiceTest {
 
         //then
         assertThat(article).isNotNull();
+    }
+
+    @DisplayName("게시글 정보 입력시 게시글 생성")
+    @Test
+    void givenArticleInfo_whenSavingArticle_thenSavesArticle() {
+        //given
+        given(articleRepository.save(any(Article.class))).willReturn(null);
+
+        //when
+        sut.saveArticle(ArticleDto.of(LocalDateTime.now(), "juno", "title", "content", "#java"));
+
+        //then
+        then(articleRepository).should().save(any(Article.class));
+    }
+
+    @DisplayName("게시글 ID와 수정 정보 입력시 게시글 수정")
+    @Test
+    void givenArticleIdAndModifiedInfo_whenUpdatingArticle_thenUpdatesArticle() {
+        //given
+        given(articleRepository.save(any(Article.class))).willReturn(null);
+
+        //when
+        sut.updateArticle(1L, ArticleUpdateDto.of( "title", "content", "#java"));
+
+        //then
+        then(articleRepository).should().save(any(Article.class));
+    }
+
+    @DisplayName("게시글 ID 입력시 게시글 삭제")
+    @Test
+    void givenArticleId_whenDeletingArticle_thenDeletesArticle() {
+        //given
+        willDoNothing().given(articleRepository).delete(any(Article.class));
+
+        //when
+        sut.deleteArticle(1L);
+
+        //then
+        then(articleRepository).should().delete(any(Article.class));
     }
 }
