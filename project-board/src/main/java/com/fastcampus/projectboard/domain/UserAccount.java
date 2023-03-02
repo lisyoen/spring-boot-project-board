8f61f808d7039bf1aeb79ee2ea656687c1d4a826
bonojuno <gtx540@naver.com>
2023-03-02 16:45:56 +0900

    #52 - 엔티티 equals(), hashcode() 비교 로직에 getter 적용
    여기서 필드에 직접 접근하면,
    하이버네이트가 지연 로딩하려고 만든 프록시 객체를 다룰 때
    필드 값이 `null`일 수 있음
    그러면 제대로 비교 로직을 수행할 수 없기 떄문에
    getter를 이용함.
    이것으로 프록시 객체를 타더라도 제대로 값을 읽어올 수 있음

--- project-board/src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
+++ project-board/src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -50,11 +50,11 @@ public class UserAccount extends AuditingFields{
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof UserAccount that)) return false;
-        return userId != null && userId.equals(that.userId);
+        return this.getUserId() != null && this.getUserId().equals(that.userId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(userId);
+        return Objects.hash(this.getUserId());
     }
 }
