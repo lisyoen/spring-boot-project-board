15693fd06f1a754ad936bf2fea7171b01d6d39f0
bonojuno <gtx540@naver.com>
2023-03-10 16:53:51 +0900

    #58 - 해시태그 서비스 구현
    게시글에 달린 해시태그를 관리하기 위한 기능을 구현
    해시태그 테스트 구현

--- project-board/src/main/java/com/fastcampus/projectboard/service/HashtagService.java
+++ project-board/src/main/java/com/fastcampus/projectboard/service/HashtagService.java
@@ -1,20 +1,53 @@
 package com.fastcampus.projectboard.service;
 
+import com.fastcampus.projectboard.domain.Hashtag;
+import com.fastcampus.projectboard.repository.HashtagRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class HashtagService {
-    public Object parseHashtagNames(String content) {
-        return null;
+    private final HashtagRepository hashtagRepository;
+
+    public Set<String> parseHashtagNames(String content) {
+        if (content == null)
+            return Set.of();
+
+        Pattern pattern = Pattern.compile("#[\\w가-힣]+");
+        Matcher matcher = pattern.matcher(content.strip());
+        Set<String> set = new HashSet<>();
+
+        while (matcher.find()) {
+            set.add(matcher.group().replace("#", ""));
+        }
+
+        return set;
     }
 
-    public Object findHashtagsByNames(Set<String> expectedHashtagNames) {
-        return null;
+    @Transactional
+    public Set<Hashtag> findHashtagsByNames(Set<String> hashtagNames) {
+        return new HashSet<>(hashtagRepository.findByHashtagNameIn(hashtagNames));
     }
 
-    public void deleteHashtagWithoutArticles(Object any) {
+    public void deleteHashtagWithoutArticles(Long hashtagId) {
+        Hashtag hashtag = hashtagRepository.getReferenceById(hashtagId);
+        if (hashtag.getArticles().isEmpty())
+            hashtagRepository.delete(hashtag);
+    }
+
+    @Transactional
+    public List<String> getAllHashtagNames() {
+        return hashtagRepository.findByHashtagNames();
     }
 
 }
